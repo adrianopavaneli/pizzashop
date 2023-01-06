bca35f9c0c93f6575c58701c092fbcb80d3677c5
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2023-01-05 22:24:33 -0300

    first commit

---
+++ src/test/java/br/com/pizzashop/PizzashopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pizzashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
