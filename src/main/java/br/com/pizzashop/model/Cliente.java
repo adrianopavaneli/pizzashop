521bf02af82a655adffffe13714673c7ae278e0f
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2023-01-14 22:33:56 -0300

    classe cliente

--- src/main/java/br/com/pizzashop/model/Cliente.java
+++ src/main/java/br/com/pizzashop/model/Cliente.java
@@ -1,65 +1,89 @@
 package br.com.pizzashop.model;
 
+import java.time.LocalDate;
+
 public class Cliente {
-	private Long id;
-	private String nome;
-	private String telefone;
-	private String endereco;
-	private String cpf;
+	private int cli_cod;
+	private String cli_nome;
+	private String cli_telefone;
+	private String cli_rua;
+	private String cli_bairro;
+	private LocalDate cli_data_cad;
 	
 	public Cliente() {
 		
 	}
 
-	public Cliente(String nome, String telefone, String endereco, String cpf) {		
-		this.nome = nome;
-		this.telefone = telefone;
-		this.endereco = endereco;
-		this.cpf = cpf;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
 	
-	
-	
-	
+	public Cliente(String cli_nome, String cli_telefone, String cli_rua, String cli_bairro, LocalDate cli_data_cad) {
+		
+		this.cli_nome = cli_nome;
+		this.cli_telefone = cli_telefone;
+		this.cli_rua = cli_rua;
+		this.cli_bairro = cli_bairro;
+		this.cli_data_cad = cli_data_cad;
+	}
 
+
+	public int getCli_cod() {
+		return cli_cod;
+	}
+
+
+	public void setCli_cod(int cli_cod) {
+		this.cli_cod = cli_cod;
+	}
+
+
+	public String getCli_nome() {
+		return cli_nome;
+	}
+
+
+	public void setCli_nome(String cli_nome) {
+		this.cli_nome = cli_nome;
+	}
+
+
+	public String getCli_telefone() {
+		return cli_telefone;
+	}
+
+
+	public void setCli_telefone(String cli_telefone) {
+		this.cli_telefone = cli_telefone;
+	}
+
+
+	public String getCli_rua() {
+		return cli_rua;
+	}
+
+
+	public void setCli_rua(String cli_rua) {
+		this.cli_rua = cli_rua;
+	}
+
+
+	public String getCli_bairro() {
+		return cli_bairro;
+	}
+
+
+	public void setCli_bairro(String cli_bairro) {
+		this.cli_bairro = cli_bairro;
+	}
+
+
+	public LocalDate getCli_data_cad() {
+		return cli_data_cad;
+	}
+
+
+	public void setCli_data_cad(LocalDate cli_data_cad) {
+		this.cli_data_cad = cli_data_cad;
+	}
+
+
+	
 }
